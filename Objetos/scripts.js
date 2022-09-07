745b12f40f36a7b5ff874929945e8dfe3356f309
mariano popolo <marianokastelic@gmail.com>
2022-09-07 17:33:23 -0300

    Add

--- Objetos/scripts.js
+++ Objetos/scripts.js
@@ -1,3 +1,146 @@
+//OBJETOS
+
+// {key: value}
+//{firstName: "Alexis"}
+
+//DECLARAR UN OBJETO en JS
+
+// const literalObject = {
+//   firstName: "Alexis",
+//   "city-residence": "Buenos Aires",
+//   job: "Developer",
+// };
+// const normalObject = new Object();
+
+// console.log(normalObject);
+
+// literalObject.favouriteColor = "Blue";
+
+// console.log(literalObject.job);
+
+// console.log(literalObject.favouriteColor);
+
+// literalObject.favouriteColor = "Red";
+
+// console.log(literalObject.favouriteColor);
+
+//clase PERSONA
+
+//3 propiedades : firstName, city, job
+
+// const firstName = "Jose";
+
+// class Persona {
+//   constructor(firstName, city, job) {
+//     this.firstName = firstName;
+//     this.city = city;
+//     this.job = job;
+//   }
+
+//   saludar() {
+//     console.log(`Hola, soy ${this.firstName}`);
+//   }
+// }
+
+// const Alexis = new Persona("Alexis", "Buenos Aires", "Developer");
+// const Juan = new Persona("Juan", "Cordoba", "Arquitecto");
+// const Martin = new Persona("Martin", "Rosario", "Abogado");
+
+// // console.log(Alexis, Juan, Martin);
+
+// // console.log(Alexis.saludar());
+// Juan.saludar();
+// Alexis.saludar();
+// Martin.saludar();
+
+//Extender una subclase desde una clase original
+// class Animal {
+//   constructor(name, isDomestic) {
+//     this.name = name;
+//     this.isDomestic = isDomestic;
+//   }
+
+//   speak() {
+//     console.log(`${this.name} hace un ruido`);
+//   }
+// }
+
+// class Dog extends Animal {
+//   constructor(name, age, isDomestic) {
+//     super(name, isDomestic);
+//     this.age = age;
+//   }
+
+//   speak() {
+//     console.log(
+//       `${this.name} ladra y ${this.isDomestic ? "es domestico" : "es salvaje"}`
+//     );
+//   }
+
+//   sayAge() {
+//     console.log(`${this.name} tiene ${this.age}`);
+//   }
+// }
+
+// const dog = new Dog("Rulo", 5, true);
+
+// // dog.speak();
+// dog.speak();
+// console.log(dog.name);
+
+//Object.key(), Object.values(),
+//Object.assign(), Object.entries()
+
+const person = {
+  firstName: "Alexis",
+  city: "Buenos Aires",
+  job: "Developer",
+};
+
+//Aceder a un elemento
+// console.log(person.firstName);
+
+//Modificar un elemento
+// person.city = "Cordoba";
+// console.log(person.city);
+
+//Object.key()
+// const keys = Object.keys(person);
+
+// keys.forEach((key) => console.log(person[key]));
+
+//For in
+// for (const key in person) {
+//   if (Object.hasOwnProperty.call(person, key)) {
+//     console.log(person[key]);
+//   }
+// }
+
+// Object.values()
+
+const values = Object.values(person);
+// console.log(values);
+
+// Object.assign()
+
+const developer = {
+  skills: "Javascript",
+  city: "Cordoba",
+};
+
+// Object.assign(person, developer);
+
+// const modifiedPerson = { ...person, ...developer };
+
+// console.log(person);
+// console.log(modifiedPerson);
+
+//Object.entries()
+
+const entries = Object.entries(person);
+
+// console.log(entries);
+
 //EJERCICIOS
 
 /*
@@ -58,3 +201,56 @@ tenis.sayIsTeamSport();
 
 basket.sayName();
 basket.sayIsTeamSport();
+
+/*2) Andrea y Martin deciden hacer un picnic al aire libre.
+Andrea propone llevar una bolsa con 2 cervezas, 2 sandwiches y 1 bolsa
+de papas fritas. Por su parte, Martin lleva 1 porcion de torta, una gaseosa
+y 1 paquete de galletitas dulces.
+CREAR UN OBJETO QUE REPRESENTE CADA UNA DE LAS BOLSAS,
+DONDE CADA PRODUCTO TENGA LA CANTIDAD QUE LLEVO CADA UNO.
+REALIZAR LAS SIGUIENTES TAREAS:
+a) Mostrar por consola los productos que llevo cada uno
+(solo el nombre del producto)
+b) En el camino al picnic, la bolsa de Andrea se rompio, 
+por lo que tuvieron que poner todo en la bolsa de Martin.
+Realizar el cambio correspondiente para que la bolsa de Martin
+tenga el total de los productos del picnic.
+c) Calcular el total de productos que llevaron al picnic
+*/
+
+class Bolsa {
+  constructor(name, producto) {
+    this.name = name;
+    this.producto = producto;
+  }
+  showProduct() {
+    console.log(`${this.name} tiene en su bolsa: ${this.producto}`);
+  }
+}
+//a)
+const bolsaAndrea = new Bolsa(
+  "Andrea",
+  "2 cervezas, 2 sandwiches y 1 bolsa de papas fritas"
+);
+
+bolsaAndrea.showProduct();
+
+const bolsaMartin = new Bolsa(
+  "Martin",
+  "1 porcion de torta, una gaseosa y 1 paquete de galletitas dulces"
+);
+
+bolsaMartin.showProduct();
+
+//b)
+
+const productMartin = Object.keys(bolsaMartin);
+const productAndrea = Object.keys(bolsaAndrea);
+
+console.log(productAndrea, productMartin);
+
+Object.assign(bolsaMartin, bolsaAndrea);
+
+console.log(bolsaAndrea);
+
+console.log(bolsaMartin);
